f9175bd63b5ca8777b419b645df6b3a9dfec26a7
akashramesh13 <akashramesh13@gmail.com>
2022-07-28 13:40:25 +0530

    complete styles and functionality

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/app.js",
+  output: {
+    path: path.join(__dirname, "public"),
+    filename: "bundle.js",
+  },
+  mode: "development",
+  module: {
+    rules: [
+      {
+        use: "babel-loader",
+        test: /\.js$/,
+        exclude: /node_modules/,
+      },
+      {
+        use: ["style-loader", "css-loader", "sass-loader"],
+        test: /\.s?css$/,
+      },
+    ],
+  },
+  devtool: "eval-cheap-module-source-map",
+  devServer: {
+    static: path.join(__dirname, "public"),
+  },
+};
