5c2ac6cde2ee4baf8ab5f55ac09440aa84b5d8cd
akashramesh13 <akashramesh13@gmail.com>
2022-08-07 17:26:33 +0530

    setup production config

--- webpack.config.js
+++ webpack.config.js
@@ -1,27 +1,31 @@
 const path = require("path");
 
-module.exports = {
-  entry: "./src/app.js",
-  output: {
-    path: path.join(__dirname, "public"),
-    filename: "bundle.js",
-  },
-  mode: "development",
-  module: {
-    rules: [
-      {
-        use: "babel-loader",
-        test: /\.js$/,
-        exclude: /node_modules/,
-      },
-      {
-        use: ["style-loader", "css-loader", "sass-loader"],
-        test: /\.s?css$/,
-      },
-    ],
-  },
-  devtool: "eval-cheap-module-source-map",
-  devServer: {
-    static: path.join(__dirname, "public"),
-  },
+module.exports = (env) => {
+  const isProduction = env.production === true;
+
+  return {
+    entry: "./src/app.js",
+    output: {
+      path: path.join(__dirname, "public"),
+      filename: "bundle.js",
+    },
+    mode: "development",
+    module: {
+      rules: [
+        {
+          use: "babel-loader",
+          test: /\.js$/,
+          exclude: /node_modules/,
+        },
+        {
+          use: ["style-loader", "css-loader", "sass-loader"],
+          test: /\.s?css$/,
+        },
+      ],
+    },
+    devtool: "eval-cheap-module-source-map",
+    devServer: {
+      static: path.join(__dirname, "public"),
+    },
+  };
 };
